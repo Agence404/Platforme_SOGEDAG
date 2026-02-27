38fbb7a153958755f9f19ed29d2cd74ac0700918
ALAAEddineACHEACHE <alaedineacheache@gmail.com>
2026-02-27 20:36:14 +0000

    Initialisation

---
+++ Aji_Tsma3_BackEnd/src/test/java/com/example/Aji_Tsma3_BackEnd/AjiTsma3BackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Aji_Tsma3_BackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AjiTsma3BackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
