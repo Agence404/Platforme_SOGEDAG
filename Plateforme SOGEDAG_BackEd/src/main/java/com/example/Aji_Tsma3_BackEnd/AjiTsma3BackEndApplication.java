278639f6b4b3e5b55dcd6bb6aa5e53ad4285d139
ALAAEddineACHEACHE <alaedineacheache@gmail.com>
2026-03-06 01:29:52 +0000

    Allo

---
+++ Plateforme SOGEDAG_BackEd/src/main/java/com/example/Aji_Tsma3_BackEnd/AjiTsma3BackEndApplication.java
@@ -0,0 +1,13 @@
+package com.example.Aji_Tsma3_BackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AjiTsma3BackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AjiTsma3BackEndApplication.class, args);
+	}
+
+}
